d7bd2844159c9fddae8280481d00e6e984eee1a8
Ethan Berman <bermane@gmail.com>
2020-02-28 08:27:33 -0700

    removed shapefile code since it didn't work with a bounding box... better to just download desired tiles and mosaic separately

--- download_temp_precip.R
+++ download_temp_precip.R
@@ -6,17 +6,7 @@ library(rgdal)
 #set wd
 setwd('/Volumes/SSD/climate_effects')
 
-#load wy shapefile to get bounding box
-wy <- readOGR('./reference/wyoming.shp')
-
-#reproject to lat lon
-wy <- spTransform(wy, '+proj=longlat +datum=WGS84 +no_defs')
-
-#change to vector and change order to match daymetr format needed
-xy <- as.vector(extent(wy))
-xy <- xy[c(4,1,3,2)]
-
-#tiles needed: queried from website if bb doesn't work
+#tiles needed: queried from website
 tiles <- c(12095, 12096, 12097, 12098,
            11915, 11916, 11917, 11918,
            11735, 11736, 11737, 11738)
